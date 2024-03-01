90cd3d3f59f2902900b1bdc3e0b0d885c4b48bb4
callbackcoding <callbackcoding08@gmail.com>
2024-03-01 12:07:36 +0500

    Video 2

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import MyComp from "./Components/MyComp";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+        perspiciatis nostrum aut eos. Dolor, in et. Corporis iste suscipit quae
+        voluptatum, animi tempore sunt totam recusandae perspiciatis adipisci.
+        Veritatis, ipsa.
+      </p>
+      <img
+        src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=sph"
+        alt=""
+      />
+
+      <MyComp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MyComp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyComp = () => {
+  return <div>My first Comp</div>;
+};
+
+export default MyComp;
